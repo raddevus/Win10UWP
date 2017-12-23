6b7194f98e72c8c0e037da99499a65633c65844d
raddeuvs <1radeutsch1@gmail.com>
2017-12-23 12:00:50 -0500

    Removing incorrect dailyjournal_v035 project.  Adding AllControls_001 and 002.  001 provides simple ContentDialog sample with AppBarButtons.  002 provides Flyout sample on OpenFile AppBarButton

---
+++ chapter_015/AllControls_v002/AllControls/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AllControls
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AppBarToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            DisplayToggleDialog();
+        }
+
+        private async void DisplayToggleDialog()
+        {
+            ContentDialog toggleDialog = new ContentDialog
+            {
+                Title = "You've Toggled the Item",
+                Content = "The Item is On",
+                CloseButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await toggleDialog.ShowAsync();
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+    }
+}
